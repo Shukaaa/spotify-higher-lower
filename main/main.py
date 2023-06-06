d962324f80d05cf9edba8230a81487af3fbeb53b
Shukaaa <asappokimane@gmail.com>
2023-06-06 11:54:46 +0200

    small improvements

--- main/main.py
+++ main/main.py
@@ -1,89 +1,47 @@
 ### imports ###
 from random import randint
-import requests
+import time
+from get_data import *
+from console_colors import *
+import os
 
-
-####################
-### Data Section ###
-####################
-
-
-### Get Endpoint Data ###
-def getDataFromEndpoint(ACCESS_TOKEN, ENDPOINT):
-    response = requests.get(
-        ENDPOINT,
-        headers = {
-            "Authorization": f"Bearer {ACCESS_TOKEN}"
-        }
-    )
-    data = response.json()
-
-    if len(data) == 1:
-        print("ERROR: WRONG AUTHKEY")
-        return None
-    else:
-        return data
-
-# Playlist Link Shorthand
-def playlistBuilder(limit, offset):
-    return f"https://api.spotify.com/v1/me/playlists?limit={limit}&offset={offset}"
-
-# Get Popularity Shorthand
-def getPopularity(data):
-    return data['items'][0]['track']['popularity']
-
-# Get Track and Artist String
-def getNameAndArtist(data):
-    name = data['items'][0]['track']['name']
-    artist = data['items'][0]['track']['album']['artists'][0]['name']
-
-    return f"{name} /// by {artist}"
-
+# clear console
+clear = lambda: os.system('cls')
 
 ####################
 ### Game Section ###
 ####################
 
 
-# LooseMSG
+##### LooseMSG #####
 def looseMSG(score):
-    print(f"""
-    You loose :(
-    Your score was: {score}
-    """)
+    print(f"\n{bcolors.FAIL}You're wrong :({bcolors.ENDC} \nYour score was: {score}\n")
 
-# WinMSG & Count Score Up
 def winMSG(score):
-    print("You're right +1")
+    print(f"\n{bcolors.OKGREEN}You're right +1{bcolors.ENDC}\n")
     return score + 1
 
-# Game Start
 def startGame(ACCESS_TOKEN, playlist_data):
     playlist = getDataFromEndpoint(ACCESS_TOKEN, f"{playlist_data['items'][0]['href']}/tracks?limit=1")
 
-    # Game Loop until loose
     score = 0
     while (1):
-        # Generates 2 random ints, retry if they're matching
         while(1):
             random1 = randint(0, playlist['total']-1)
             random2 = randint(0, playlist['total']-1)
             if random1 != random2:
                 break
         
-        # Get Track-Data from ranInts
         track1 = getDataFromEndpoint(ACCESS_TOKEN, f"{playlist_data['items'][0]['href']}/tracks?limit=1&offset={random1}")
         track2 = getDataFromEndpoint(ACCESS_TOKEN, f"{playlist_data['items'][0]['href']}/tracks?limit=1&offset={random2}")
 
-        # Print Score and Track Name + Artist
-        print(f"Score: {score}")
+
+        print(f"{bcolors.HEADER}Score: {score}{bcolors.ENDC}")
         print(f"""Wich Track is more popular: 
-        1. {getNameAndArtist(track1)}
-        2. {getNameAndArtist(track2)}
+        {bcolors.HEADER}1{bcolors.ENDC}. {getNameAndArtist(track1)}
+        {bcolors.HEADER}2{bcolors.ENDC}. {getNameAndArtist(track2)}
         """)
-        
-        # Get the guess and check if its greater then the other one
-        choice = input("Type 1 or 2: ")
+        choice = input(f"Type {bcolors.HEADER}1{bcolors.ENDC} or {bcolors.HEADER}2{bcolors.ENDC}: ")
         if choice == "1":
             if getPopularity(track1) >= getPopularity(track2):
                 score = winMSG(score)
@@ -116,11 +74,14 @@ def main():
     maxCount = 0
     for i in range(0, data_playlist['total']):
         d = getDataFromEndpoint(ACCESS_TOKEN, playlistBuilder(1, i))
-        print(f"ID: {i} | {d['items'][0]['name']}")
-        maxCount = i
+        if d['items'][0]['name'] != "":
+            print(f"ID: {i} | {d['items'][0]['name']}")
+        maxCount = i-1
 
-    ### Choose Playlist  ###
+    maxCount = maxCount + 1
+
     while(1):
+        ### Choose Playlist  ###
         playlist_id = input("Type ID of the Playlist you want to choose: ")
 
         if playlist_id.isnumeric():
@@ -132,20 +93,21 @@ def main():
         else:
             print("Input not a number")
     
-    # Get Data from choosen Playlist
+    
     playlist_data = getDataFromEndpoint(ACCESS_TOKEN, playlistBuilder(1, playlist_id))
     print(f"You choose: {playlist_data['items'][0]['name']}")
 
-    # Game Loop for retry
+    time.sleep(1)
+    clear()
+
     while(1):
         startGame(ACCESS_TOKEN, playlist_data)
-        retry = input("""
-        Do you want to retry?
-        Type <r> if you want
-        """)
+        retry = input("Do you want to retry?\nType <r> if you want\n")
         if retry != "r":
             break
-
+        else:
+            clear()
+    
 
 ### Start ###
 if __name__ == '__main__':
--- start.bat
+++ start.bat
@@ -1,27 +1,4 @@
 @ECHO OFF
-TITLE Spotify Games 
+TITLE Spotify Higher Lower Game 
 
-SET /P LANG="Choose the Language: (PythonCMD, PythonGUI): "
-
-IF "%LANG%" == "PythonCMD" (
-    TITLE Spotify Games / PythonCMD
-    ECHO Check if Python is installed:
-    python --version
-    ECHO Click ANY key when Python is installed
-    PAUSE
-    python main/python/main.py
-)
-
-IF "%LANG%" == "PythonGUI" (
-    TITLE Spotify Games / PythonGUI
-    ECHO Check if Python is installed:
-    python --version
-    ECHO Click ANY key when Python is installed
-    PAUSE
-    ECHO Installing PySimpleGUI Package
-    pip install pysimplegui
-    ECHO Click ANY key when PySimpleGUI got installed
-    PAUSE
-    ECHO If error happen try install PySimpleGUI manually
-    python main/python/main_gui.py
-)
+python main/main.py
